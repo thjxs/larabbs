97689c0d8a654de7db3a07dbb70b92ca1a0ab3c0
thjxs <tanhongjianx@163.com>
2018-04-04 11:59:19 +0000

    create categories

---
+++ database/migrations/2018_04_04_195008_seed_categories_data.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name' => 'share',
+                'description' => 'create and descover',
+            ],
+            [
+                'name' => 'cursor',
+                'description' => 'skill, develop',
+            ],
+            [
+                'name' => 'board',
+                'description' => 'state',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
