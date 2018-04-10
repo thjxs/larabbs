1622fbd5c4c401a71beefe7fe630249837a5d776
thjxs <tanhongjianx@163.com>
2018-04-10 23:38:19 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,12 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
 ], function ($api) {
-    $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
-    $api->post('users', 'UsersController@store')->name('api.users.store');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+        $api->post('users', 'UsersController@store')->name('api.users.store');
+    });
 });
