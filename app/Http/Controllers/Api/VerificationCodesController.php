c93897e1dcf53aa48f4f37407002e5ca09726015
thjxs <tanhongjianx@163.com>
2018-04-10 17:48:28 +0800

    verificationCode store

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use App\Http\Requests\Api\VerificationCodeRequest;
+
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request, EasySms $easySms)
+    {
+        $phone = $request->phone;
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+            try {
+                $result = $easySms->send($phone, [
+                    'content' => "【lara社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
+                ]);
+            } catch (\GuzzleHttp\Exception\ClientException $exception) {
+                $response = $exception->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? 'sms send error');
+            }
+        }
+
+        $key = 'verificationCode_' . str_random(15);
+        $expiredAt = now()->addMinutes(10);
+
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
