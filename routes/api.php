04611df2039a0f8ab9707e08668feb347f7d3941
thjxs <tanhongjianx@163.com>
2018-04-13 16:53:49 +0800

    notifications read

--- routes/api.php
+++ routes/api.php
@@ -52,6 +52,7 @@ $api->version('v1', [
             $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')->name('api.topics.replies.destroy');
             $api->get('user/notifications', 'NotificationsController@index')->name('api.user.notifications.index');
             $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
+            $api->patch('user/read/notifications', 'NotificationsController@read')->name('api.user.notifications.read');
         });
     });
 });
