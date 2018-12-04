018e4c46b3e1b4d62e011c7913d616e0b6fb21cd
thjxs <tanhongjianx@163.com>
2018-12-05 05:31:57 +0800

    Update policy

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -15,8 +15,6 @@ class Policy
 
     public function before($user, $ability)
     {
-        if ($user->can('manage_contents')) {
-            return true;
-        }
+        return $user->can('manage_contents');
     }
 }
